2a632c673debe3558cf6b4026cd1bebc0600c7a5
Nicolas Orjuela <n.orjuela@uniandes.edu.co>
2022-05-07 17:34:17 -0500

    API - 6 endpoints v1

--- API/main.py
+++ API/main.py
@@ -19,18 +19,36 @@ def read_item(item_id: int, q: Optional[str] = None):
    return {"item_id": item_id, "q": q}
 
 
-@app.post("/predict")
+@app.post("/logisticregression/predict")
 def make_predictions(dataModel: DataModel.DataModel):
     df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
     df.columns = dataModel.columns()
-    model = load("modelo.joblib")
+    model = load("logisticregression.joblib")
+    result = model.predict(df)
+    print(model)
+    return result.tolist()
+
+@app.post("/naivebayes/predict")
+def make_predictions(dataModel: DataModel.DataModel):
+    df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
+    df.columns = dataModel.columns()
+    model = load("naivebayes.joblib")
+    result = model.predict(df)
+    print(model)
+    return result.tolist()
+
+@app.post("/svm/predict")
+def make_predictions(dataModel: DataModel.DataModel):
+    df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
+    df.columns = dataModel.columns()
+    model = load("svm.joblib")
     result = model.predict(df)
     print(model)
     return result.tolist()
 
 cols = ["label", "study_and_condition"]
 
-@app.post("/getrsquared")
+@app.post("/logisticregression/getrsquared")
 def get_r2(dataList: DataModel.DataList):
     df = pd.DataFrame(columns=cols)
     for data in dataList.data:
@@ -38,7 +56,31 @@ def get_r2(dataList: DataModel.DataList):
     print(df)
     x = df.drop("label", axis=1)
     y = df["label"]
-    model = load("modelo.joblib")
+    model = load("logisticregression.joblib")
+    result = model.score(x, y)
+    return result
+
+@app.post("/naivebayes/getrsquared")
+def get_r2(dataList: DataModel.DataList):
+    df = pd.DataFrame(columns=cols)
+    for data in dataList.data:
+        df = df.append(pd.DataFrame(np.array(list(data.dict().values())).reshape(1,-1), columns=cols), ignore_index=True)
+    print(df)
+    x = df.drop("label", axis=1)
+    y = df["label"]
+    model = load("naivebayes.joblib")
+    result = model.score(x, y)
+    return result
+
+@app.post("/svm/getrsquared")
+def get_r2(dataList: DataModel.DataList):
+    df = pd.DataFrame(columns=cols)
+    for data in dataList.data:
+        df = df.append(pd.DataFrame(np.array(list(data.dict().values())).reshape(1,-1), columns=cols), ignore_index=True)
+    print(df)
+    x = df.drop("label", axis=1)
+    y = df["label"]
+    model = load("svm.joblib")
     result = model.score(x, y)
     return result
 
